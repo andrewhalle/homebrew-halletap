d81053a98c34f6ec1dcd8c34ad32a23795564296
Andrew Halle <ahalle@berkeley.edu>
2018-09-12 22:37:11 -0700

    update download url

--- todo.rb
+++ todo.rb
@@ -1,7 +1,7 @@
 class Todo < Formula
   desc "decide what to do next using your favorite scheduling algorithm"
   homepage "https://github.com/andrewhalle/todo"
-  url "https://s3-us-west-1.amazonaws.com/halletap-archive/todo/v0.0/b0/todo"
+  url "https://s3-us-west-1.amazonaws.com/halletap-archive/todo/v0.0/b0/todo-mac-0.0.0.tar.gz"
   sha256 "62f71340578a865bad48053a36c02cb4d6a38f49f0b04ab7fb24f269bd194170"
   version "0.0.0"
 
