f666fe118c9010e690fa138e949075c459907409
Andrew Halle <ahalle@berkeley.edu>
2018-09-16 10:47:29 -0700

    todo v0.0.1

--- todo.rb
+++ todo.rb
@@ -1,9 +1,9 @@
 class Todo < Formula
   desc "decide what to do next using your favorite scheduling algorithm"
   homepage "https://github.com/andrewhalle/todo"
-  url "https://s3-us-west-1.amazonaws.com/halletap-archive/todo/v0.0/b0/todo-mac-0.0.0.tar.gz"
-  sha256 "dedb37bc40cbc4483394cd507eaf69ad0ac2282a8309c54aec137378cef1fc8e"
-  version "0.0.0"
+  url "https://s3-us-west-1.amazonaws.com/halletap-archive/todo/v0.0/todo-mac-0.0.1.tar.gz"
+  sha256 "a2cb08d33b81733206f56bd7dfd71688ce34e1c7e2aa7bd911c75bb353a257ea"
+  version "0.0.1"
 
   bottle :unneeded
 
